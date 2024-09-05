8a71df53e8f22b5008842ff4eb942a871ea2fbb5
DuongNguyen.eth <trungduong23@gmail.com>
2024-09-05 01:33:14 -0400

    fixed

--- backend/charts/views.py
+++ backend/charts/views.py
@@ -1,18 +1,8 @@
-from rest_framework.decorators import api_view
+# charts/views.py
+
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Candlestick Chart Data
-@api_view(['GET'])
-def candlestick_data(request):
-    data = {
-        "data": [
-            {"x": "2023-01-01", "open": 30, "high": 40, "low": 25, "close": 35},
-            {"x": "2023-01-02", "open": 35, "high": 45, "low": 30, "close": 40},
-        ]
-    }
-    return Response(data)
-
-# Line Chart Data
 @api_view(['GET'])
 def line_chart_data(request):
     data = {
@@ -21,7 +11,6 @@ def line_chart_data(request):
     }
     return Response(data)
 
-# Bar Chart Data
 @api_view(['GET'])
 def bar_chart_data(request):
     data = {
@@ -30,11 +19,21 @@ def bar_chart_data(request):
     }
     return Response(data)
 
-# Pie Chart Data
 @api_view(['GET'])
 def pie_chart_data(request):
     data = {
         "labels": ["Red", "Blue", "Yellow"],
         "data": [300, 50, 100]
+    }
+    return Response(data)
+
+@api_view(['GET'])
+def candlestick_data(request):
+    data = {
+        "data": [
+            {"x": "2023-01-01", "y": [30, 40, 25, 35]},  # Correct format: [open, high, low, close]
+            {"x": "2023-01-02", "y": [35, 45, 30, 40]},
+            {"x": "2023-01-03", "y": [40, 50, 35, 45]},
+        ]
     }
     return Response(data)
